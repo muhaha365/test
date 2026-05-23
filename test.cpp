fee2c44f73171be253e7419912317af357f883f6
Nguyen Tuan Long <email_cua_ban@gmail.com>
2026-05-23 11:30:43 +0700

    redo

--- test.cpp
+++ test.cpp
@@ -1,163 +1,77 @@
 #include <iostream>
 #include <vector>
-#include <algorithm>
 using namespace std;
 
 class CauThu
 {
 private:
-    string TenCauThu, ViTriDa;
-    int SoAo;
+    string tenCT, ViTri;
+    int soao;
 public:
-    CauThu(string ten = "", string vitri = "", int so = 0): TenCauThu(ten), ViTriDa(vitri), SoAo(so) {}
-    void Nhap()
-    {
-        cin.ignore();
-        cout << "Ten cau thu: ";
-        getline(cin, TenCauThu);
-        cout << "Vi tri da: ";
-        getline(cin, ViTriDa);
-        cout << "So ao: ";
-        cin >> SoAo;
-    }
+    CauThu(string ten = "", string vitri = "", int so = 0): tenCT(ten), ViTri(vitri), soao(so) {}
     void Xuat()
     {
-        cout << "Ten: " << TenCauThu << endl;
-        cout << "Vi tri: " << ViTriDa << endl;
-        cout << "So ao: " << SoAo << endl;
+        cout << "Ten cau thu: " << tenCT << " | " << ViTri << " | " << soao;
     }
 };
 
 class DoiBong
 {
 private:
-    vector<CauThu> dsCT;
-    string ten_doi, ten_hlv;
-    int tong_diem, so_tran_da, tran_thang, tran_thua, tran_hoa, ban_thang, ban_thua, hieu_so;
+    string tendoi;
+    string tenHLV;
+    vector <CauThu> dsCT;
+    int tong_diem, sotran, thang, hoa, thua, banThang, banThua;
 public:
-    DoiBong(string ten_d = "", string ten_h = "", int tong = 0, int so_tran = 0, int thang = 0, int thua = 0, int hoa = 0, int ban_win = 0, int ban_lose = 0, int hieu = 0):
-    ten_doi(ten_d), ten_hlv(ten_h), tong_diem(tong), so_tran_da(so_tran), tran_thang(thang), tran_thua(thua), tran_hoa(hoa), ban_thang(ban_win), ban_thua(ban_lose), hieu_so(hieu) {}
-    void Nhap()
+    DoiBong(string ten = "", string hlv =""): tendoi(ten), tenHLV(hlv)
     {
-        cin.ignore();
-        cout << "Nhap ten doi: ";
-        getline(cin, ten_doi);
-        cout << "Nhap ten HLV: ";
-        getline(cin, ten_hlv);
-        int n;
-        cout << "Nhap so cau thu: ";
-        cin >> n;
-        dsCT.resize(n);
-        for(int i = 0; i < dsCT.size(); i++)
+        tong_diem = sotran = thang = hoa = thua = banThang = banThua = 0;
+    }
+    void ThemCauThu(CauThu ct)
+    {
+        dsCT.push_back(ct);
+    }
+    void CapNhat(int sobanthang, int sobanthua)
+    {
+        sotran++;
+        banThang += sobanthang;
+        banThua += sobanthua;
+        if(sobanthang > sobanthua)
         {
-            dsCT[i].Nhap();
+            thang++;
+            tong_diem += 3;
         }
-    }
-    void Xuat()
-    {
-        cout << "Ten doi: " << ten_doi << endl;
-        cout << "Ten HVL: " << ten_hlv << endl;
-        for(int i = 0; i < dsCT.size(); i++)
+        else if(sobanthang == sobanthua)
         {
-            dsCT[i].Xuat();
-        }
-    }
-    void CongTranThang(int tran)
-    {
-        tran_thang += tran;
-    }
-    void CongTranThua(int tran)
-    {
-        tran_thua += tran;
-    }
-    void CongTranHoa(int tran)
-    {
-        tran_hoa += tran;
-    }
-    void CongDiem(int diem)
-    {
-        tong_diem += diem;
-    }
-    void Congbanthang(int ban)
-    {
-        ban_thang += ban;
-    }
-    void Congbanthua(int ban)
-    {
-        ban_thua += ban;
-    }
-    int HieuSo()
-    {
-        return ban_thang - ban_thua;
-    }
-};
-
-class TranDau
-{
-private:
-    DoiBong a, b;
-public:
-    void NhapKetQua(int ban_thang_a, int ban_thang_b)
-    {
-        if(ban_thang_a > ban_thang_b)
-        {
-            a.CongDiem(3);
-            a.CongTranThang(1);
-            a.Congbanthang(ban_thang_a);
-            a.Congbanthua(ban_thang_b);
-            b.CongDiem(0);
-            b.CongTranThua(1);
-            b.Congbanthang(ban_thang_b);
-            b.Congbanthua(ban_thang_a);
-        }
-        else if(ban_thang_a < ban_thang_b)
-        {
-            b.CongDiem(3);
-            b.CongTranThang(1);
-            b.Congbanthang(ban_thang_a);
-            b.Congbanthua(ban_thang_b);
-            a.CongDiem(0);
-            a.CongTranThua(1);
-            a.Congbanthang(ban_thang_b);
-            a.Congbanthua(ban_thang_a);
+            hoa++;
+            tong_diem += 1;
         }
         else
         {
-            a.CongDiem(1);
-            b.CongDiem(1);
-            a.Congbanthang(ban_thang_a);
-            a.Congbanthua(ban_thang_b);
-            b.Congbanthang(ban_thang_b);
-            b.Congbanthua(ban_thang_a);
-            a.CongTranHoa(1);
-            b.CongTranHoa(1);
+            thua++;
         }
     }
-};
-
-class GiaiDau
-{
-private:
-    vector <DoiBong> ds;
-public:
-    GiaiDau() {}
-    void nhapDsDoi()
+    int getHieuSo()
     {
-        int n;
-        cout << "So doi: ";
-        cin >> n;
-        ds.resize(n);
-        for(int i = 0; i < ds.size(); i++)
-        {
-            ds[i].Nhap();
-        }
+        return banThang - banThua;
+    }
+    int gettong_diem() const
+    {
+        return tong_diem;
+    }
+    int getbanThang() const
+    {
+        return banThang;
+    }
+    string gettenDoi() const
+    {
+        return tendoi;
+    }
+    void Xuat() 
+    {
+        cout << "Doi: " << tendoi << " | Diem: " << tong_diem 
+             << " | Tran: " << sotran << " | T-H-B: " << thang << "-" << hoa << "-" << thua 
+             << " | BT-BB: " << banThang << "-" << banThua 
+             << " | Hieu so: " << getHieuSo() << endl;
     }
 };
-
-int main()
-{
-    int n;
-    cin >> n;
-    cout << "nhap doi bong: ";
-    return 0;
-}
